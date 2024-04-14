61420c5a7c0e062362ebd2ccd472e9b13caba507
Osman Fikret Ceylan <ofcyln@gmail.com>
2024-04-14 18:22:40 +0200

    refactor: logo URL

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <div>
         <a href="#">
-          <img src="/public/assets/logo/logo.svg" className="logo" alt="Logo" />
+          <img src="logo.svg" className="logo" alt="Logo" />
         </a>
       </div>
 
